dbf13da6952e1cfd87e3e23457bf9785c1ce3ff5
RidwanRizqi <ridwanrizqi64@gmail.com>
2023-08-25 09:04:36 +0700

    variables

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -1,23 +1,28 @@
 import 'package:flutter/material.dart';
 import 'package:first_app/styled_text.dart';
 
+var startAlignment = Alignment.topCenter;
+var endAlignment = Alignment.bottomCenter;
+
 class GradientContainer extends StatelessWidget {
   const GradientContainer({super.key});
 
   @override
   Widget build(context) {
     return Container(
-      decoration: const BoxDecoration(
+      decoration: BoxDecoration(
         gradient: LinearGradient(
-          colors: [
+          colors: const [
             Colors.blue,
             Colors.lime,
           ],
-          begin: Alignment.topCenter,
-          end: Alignment.bottomCenter,
+          begin: startAlignment,
+          end: endAlignment,
         ),
       ),
-      child: const Center(child: StyledText()),
+      child: const Center(
+        child: StyledText(),
+      ),
     );
   }
 }
